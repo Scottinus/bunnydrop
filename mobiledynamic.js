beb8101fddf078020a8105f5fc15926e94766b74
scottinus <sorace.lc@gmail.com>
2023-02-02 11:30:53 +0100

    new script

--- mobiledynamic.js
+++ mobiledynamic.js
@@ -1,63 +1,55 @@
-/* function onClick() {
-  console.log("REQUEST PERMISSION");
-  if (typeof DeviceMotionEvent.requestPermission === "function") {
-    // Handle iOS 13+ devices.
-    DeviceMotionEvent.requestPermission()
-      .then((state) => {
-        if (state === "granted") {
-          window.addEventListener("devicemotion", handleOrientation);
-        } else {
-          console.error("Request to access the orientation was rejected");
-        }
-      })
-      .catch(console.error);
-  } else {
-    // Handle regular non iOS 13+ devices.
-    window.addEventListener("devicemotion", handleOrientation);
-  }
-} */
+function onDeviceOrientation(event) {
+  const btn = document.querySelector("button");
+  btn.style.backgroundColor = "green";
+  btn.innerText = "enabled";
+  console.log("ON DEVICE OREINTATION");
+  console.log(`event`, event);
+  /*     setCssTransformInverse({
+  transform: `rotateX(${b}deg) rotateY(${g}deg) rotateZ(${a}deg)`,
+}); */
+  var absolute = event.absolute;
+  var alpha = event.alpha;
+  // alpha is the inclination on the z-axis
 
-window.addEventListener(
-  "deviceorientation",
-  function (event) {
-    var absolute = event.absolute;
-    var alpha = event.alpha;
-    // alpha is the inclination on the z-axis
+  var beta = event.beta;
+  // beta is the inclination on the x-axis
 
-    var beta = event.beta;
-    // beta is the inclination on the x-axis
+  var gamma = event.gamma;
+  // gamma is the inclination on the y-axis
 
-    var gamma = event.gamma;
-    // gamma is the inclination on the y-axis
-
-    console.log(beta);
-    images.forEach((image, index) => {
-      {
-        let speed = 2;
-        switch (index) {
-          case 3:
-            speed = 2;
-            break;
-          case 2:
-            speed = 3;
-            break;
-          default:
-            speed = 3.5;
-            break;
-        }
-        const xRotation = () => {
-          let rotation = (beta - 90) / (speed * 10);
-          return -rotation;
-        };
-        const xPosition = gamma / (speed * 50);
-        const yPosition = beta / (speed * 50);
-        image.style.transform = `translate(-50%) perspective(50em) rotateX(${xRotation()}deg) rotateY(${
-          gamma / (speed * 10)
-        }deg)`;
-        image.style.left = `${xPosition + 50}%`;
-        image.style.top = `${yPosition}%`;
+  console.log(beta);
+  images.forEach((image, index) => {
+    {
+      let speed = 2;
+      switch (index) {
+        case 3:
+          speed = 2;
+          break;
+        case 2:
+          speed = 3;
+          break;
+        default:
+          speed = 3.5;
+          break;
       }
-    });
-  },
-  true
-);
+      const xRotation = () => {
+        let rotation = (beta - 90) / (speed * 10);
+        return -rotation;
+      };
+      const xPosition = gamma / (speed * 50);
+      const yPosition = beta / (speed * 50);
+      image.style.transform = `translate(-50%) perspective(50em) rotateX(${xRotation()}deg) rotateY(${
+        gamma / (speed * 10)
+      }deg)`;
+      image.style.left = `${xPosition + 50}%`;
+      image.style.top = `${yPosition}%`;
+    }
+  });
+}
+
+function requestOrientationPermission() {
+  DeviceOrientationEvent.requestPermission();
+}
+const onClick = async () => {
+  window.addEventListener("deviceorientation", onDeviceOrientation, true);
+};
---
+++ script.js
@@ -0,0 +1,83 @@
+/*  created by Henry Zhang
+    coded at 12/Jun/2019    */
+
+//artwork: trench by twenty one pilots
+var imgUrl = "";
+const cards = [...document.querySelectorAll(".card")];
+updateReflection(100, 0);
+
+window.addEventListener("mousemove", (event) => {
+  let mouseX = event.clientX;
+  let mouseY = event.clientY;
+  let halfWidth = window.innerWidth / 2;
+  let halfHeight = window.innerHeight / 2;
+  let xdeg = (mouseX - halfWidth) / halfWidth;
+  let ydeg = (mouseY - halfHeight) / halfHeight;
+  let speed = 2;
+  updateReflection(ydeg * 180, xdeg * 100);
+  cards.forEach((card, index) => {
+    switch (index) {
+      //frame
+      case 3:
+        speed = 10;
+        break;
+      //shadow frame
+      case 2:
+        speed = 16;
+        break;
+      //character
+      case 1:
+        speed = 4;
+        break;
+      //bg
+      case 0:
+        speed = 4;
+        break;
+    }
+    card.style.transform = `rotateX(${ydeg * speed}deg) rotateY(${
+      xdeg * speed * 2
+    }deg)`;
+  });
+});
+
+window.ondevicemotion = function (event) {
+  var accelerationX = event.accelerationIncludingGravity.x;
+  var accelerationY = event.accelerationIncludingGravity.y;
+  var accelerationZ = event.accelerationIncludingGravity.z;
+  // console.log(`${accelerationX},${accelerationY},${accelerationZ}`)
+  let xdeg = accelerationX / 10;
+  let ydeg = accelerationY / 10;
+  updateReflection(ydeg * 180, xdeg * 100);
+  cards.forEach((card, index) => {
+    switch (index) {
+      //frame
+      case 3:
+        speed = 10;
+        break;
+      //shadow frame
+      case 2:
+        speed = 16;
+        break;
+      //character
+      case 1:
+        speed = 4;
+        break;
+      //bg
+      case 0:
+        speed = 4;
+        break;
+    }
+    card.style.transform = `rotateX(${ydeg * speed}deg) rotateY(${
+      xdeg * speed
+    }deg)`;
+  });
+};
+
+function updateReflection(degree, percentage) {
+  cards.forEach((card, index) => {
+    if (index === 3) {
+      card.style.background = `linear-gradient(${degree}deg, rgba(255,255,255,0) 0%,rgba(255,255,255,0.4) ${percentage}%,rgba(255,255,255,0) 100%), url('${""}')`;
+      card.style.backgroundSize = "cover";
+    }
+  });
+}
