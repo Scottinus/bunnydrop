a82836242ccc6ea82c9ef7d76e5a73f6c049d4c6
scottinus <sorace.lc@gmail.com>
2023-02-03 17:00:56 +0100

    new code

--- script.js
+++ script.js
@@ -1,140 +1,170 @@
-/*  created by Henry Zhang
-    coded at 12/Jun/2019    */
+// change the animation state of the second image when clicking on it
+const doors = document.querySelector(".frames");
+const knockAction = document.querySelector("#knock-action");
+const turnLightAction = document.querySelector("#light-action");
+const wakeAction = document.querySelector("#wake-action");
+const regretAction = document.querySelector("#regret-action");
+const apologizeAction = document.querySelector("#apologize-action");
+const restedAction = document.querySelector("#rested-action");
+const endAction = document.querySelector("#end-action");
+const omgAction = document.querySelector("#omg-action");
+const gif = document.querySelector(".img-gif");
+const imageContainer = document.querySelector(".image-container");
+const body = document.querySelector("body");
+const heartbox = document.querySelector(".heartbeat-box");
+const noisesBox = [...document.querySelectorAll(".noises-box")];
+const imageFirstOpenSources = ["d1.png", "d3.png", "d4.png"];
+const imageSecondSources = ["d5.png", "d6.png", "d7.png", "d8.png"];
+const imageSources = [
+  "d1.png",
+  "d3.png",
+  "d4.png",
+  "d5.png",
+  "d6.png",
+  "d7.png",
+  "d8.png",
+];
+let currentImageIndex = 0;
+let intervalId = null;
+let closeDoorIntervalId = null;
+let retryCounter = 0;
 
-//artwork: trench by twenty one pilots
-var imgUrl = "";
-const cards = [...document.querySelectorAll(".card")];
-updateReflection(100, 0);
-
-function isMobileDevice() {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  );
+function handleHalfAnimation() {
+  doors.src = "./Bunnydrop/" + imageFirstOpenSources[currentImageIndex];
+  currentImageIndex = (currentImageIndex + 1) % imageFirstOpenSources.length;
+  if (currentImageIndex === 0) {
+    clearInterval(intervalId);
+  }
 }
-const isMobile = isMobileDevice();
-if (isMobile) {
-  console.log("IN MOBILE", isMobile);
-  let initialX;
-  let initialY;
-  let currentX;
-  let currentY;
-
-  window.addEventListener("touchstart", function (event) {
-    initialX = event.touches[0].clientX;
-    initialY = event.touches[0].clientY;
-  });
-  window.addEventListener("touchend", function (event) {
-    updateReflection(180, 100);
-    cards.forEach((card, index) => {
-      card.style.transform = `rotateX(${0}deg) rotateY(${0}deg)`;
-    });
-  });
-
-  window.addEventListener("touchmove", function (event) {
-    currentX = event.touches[0].clientX;
-    currentY = event.touches[0].clientY;
-    let halfWidth = window.innerWidth / 2;
-    let halfHeight = window.innerHeight / 2;
-    const xdeg = (initialX - currentX - halfWidth) / halfWidth;
-    const ydeg = (initialY - currentY - halfHeight) / halfHeight;
-    let customRotateX;
-    let customRotateY;
-    updateReflection(ydeg * 180, xdeg * 100 * 2);
-    cards.forEach((card, index) => {
-      switch (index) {
-        //frame
-        case 3:
-          speed = 4;
-          customRotateX = -ydeg * speed;
-          customRotateY = xdeg * speed * 1.5;
-          break;
-        //shadow frame
-        case 2:
-          speed = 8;
-          customRotateX = ydeg * speed;
-          customRotateY = -xdeg * speed * 1.5;
-          break;
-        //character
-        case 1:
-          speed = 2;
-          customRotateX = ydeg * speed;
-          customRotateY = -xdeg * speed * 1.5;
-          break;
-        //bg
-        case 0:
-          speed = 3;
-          customRotateX = ydeg * speed * 1.5;
-          customRotateY = -xdeg * speed * 1.5;
-          break;
-      }
-
-      function handleRange(num, min, max) {
-        if (num < min) {
-          return min;
-        } else if (num > max) {
-          return max;
-        } else {
-          return num;
-        }
-      }
-
-      card.style.transform = `rotateX(${handleRange(
-        customRotateX,
-        -8,
-        8
-      )}deg) rotateY(${handleRange(customRotateY, -8, 8)}deg)`;
-    });
-  });
-} else {
-  console.log("NOT MOBILE", isMobile);
-  window.addEventListener("mousemove", (event) => {
-    let mouseX = event.clientX;
-    let mouseY = event.clientY;
-    let halfWidth = window.innerWidth / 2;
-    let halfHeight = window.innerHeight / 2;
-    let xdeg = (mouseX - halfWidth) / halfWidth;
-    let ydeg = (mouseY - halfHeight) / halfHeight;
-    let speed = 2;
-    let customRotateX = ydeg * speed;
-    let customRotateY = xdeg * speed;
-    updateReflection(ydeg * 180, xdeg * 100);
-    cards.forEach((card, index) => {
-      switch (index) {
-        //frame
-        case 3:
-          speed = 2;
-          customRotateX = -ydeg * speed;
-          customRotateY = xdeg * speed;
-          break;
-        //shadow frame
-        case 2:
-          speed = 4;
-          customRotateX = -ydeg * speed;
-          customRotateY = xdeg * speed;
-          break;
-        //character
-        case 1:
-          speed = 1;
-          customRotateX = ydeg * speed;
-          customRotateY = -xdeg * speed;
-          break;
-        //bg
-        case 0:
-          speed = 1;
-          customRotateX = ydeg * speed;
-          customRotateY = -xdeg * speed;
-          break;
-      }
-      card.style.transform = `rotateX(${customRotateX}deg) rotateY(${customRotateY}deg)`;
-    });
-  });
+function handleSecondHalfAnimation() {
+  doors.src = "./Bunnydrop/" + imageSecondSources[currentImageIndex];
+  currentImageIndex = (currentImageIndex + 1) % imageSecondSources.length;
+  if (currentImageIndex === 0) {
+    clearInterval(intervalId);
+    console.log("closing door");
+    setTimeout(() => {
+      closeDoorIntervalId = setInterval(closeDoor, 250);
+    }, 1500);
+  }
 }
 
-function updateReflection(degree, percentage) {
-  cards.forEach((card, index) => {
-    if (index === 3) {
-      card.style.background = `linear-gradient(${degree}deg, rgba(255,255,255,0) 0%,rgba(255,255,255,0.5) ${percentage}%,rgba(255,255,255,0) 100%)`;
-      card.style.backgroundSize = "cover";
+function closeDoor() {
+  doors.src = "./Bunnydrop/" + imageSources.reverse()[currentImageIndex];
+  currentImageIndex = (currentImageIndex + 1) % imageSources.reverse().length;
+  if (currentImageIndex === 0) {
+    clearInterval(closeDoorIntervalId);
+    body.style.background = "#1b1b1b";
+    imageContainer.style.opacity = 0;
+    heartbox.style.display = "block";
+    noisesBox.forEach((elem, index) => {
+      elem.style.display = "block";
+    });
+    setTimeout(() => {
+      restedAction.top = "65%";
+      restedAction.style.opacity = 1;
+      restedAction.style.display = "block";
+    }, 3000);
+  }
+}
+
+function openFullHalfDoor() {
+  doors.src = "./Bunnydrop/" + imageSources[currentImageIndex];
+  currentImageIndex = (currentImageIndex + 1) % imageSources.length;
+  if (currentImageIndex === 0) {
+    clearInterval(intervalId);
+  }
+}
+
+function ActivateDoor() {
+  knockAction.style.display = "none";
+  gif.style.display = "none";
+  intervalId = setInterval(handleHalfAnimation, 250);
+  setTimeout(() => {
+    wakeAction.style.display = "block";
+    wakeAction.style.opacity = 1;
+  }, 1000);
+}
+
+function turnOnLight() {
+  turnLightAction.style.opacity = 0;
+  body.style.background = "#eee";
+  turnLightAction.style.display = "none";
+  imageContainer.style.opacity = 1;
+  setTimeout(() => {
+    knockAction.style.display = "block";
+    knockAction.style.opacity = 1;
+  }, 1000);
+}
+
+function apologize() {
+  wakeAction.style.opacity = 0;
+  wakeAction.style.display = "none";
+  regretAction.style.opacity = 0;
+  regretAction.style.display = "none";
+  apologizeAction.style.opacity = 1;
+  apologizeAction.style.display = "block";
+}
+
+function comfortable() {
+  wakeAction.style.opacity = 0;
+  wakeAction.style.display = "none";
+  regretAction.style.opacity = 1;
+  regretAction.style.display = "block";
+}
+
+function openAllDoor() {
+  gif.style.display = "none";
+  regretAction.style.opacity = 0;
+  regretAction.style.display = "none";
+  apologizeAction.style.opacity = 0;
+  apologizeAction.style.display = "none";
+  intervalId = setInterval(handleSecondHalfAnimation, 250);
+}
+
+function restedEnough() {
+  restedAction.style.opacity = 0;
+  restedAction.style.display = "none";
+  heartbox.style.display = "none";
+  noisesBox.forEach((elem, index) => {
+    elem.style.display = "none";
+  });
+  imageContainer.style.opacity = 1;
+  body.style.background = "#eee";
+  intervalId = setInterval(openFullHalfDoor, 250);
+  setTimeout(() => {
+    console.log("RETRY COUNTER", retryCounter);
+    if (retryCounter < 3) {
+      endAction.style.opacity = 1;
+      endAction.style.display = "block";
+    } else {
+      omgAction.style.opacity = 1;
+      omgAction.style.display = "block";
     }
-  });
+  }, 2000);
+}
+
+function repeat() {
+  retryCounter = retryCounter + 1;
+  endAction.style.opacity = 0;
+  endAction.style.display = "none";
+  closeDoorIntervalId = setInterval(closeDoor, 250);
+}
+function reset() {
+  turnLightAction.style.opacity = 1;
+  knockAction.style.opacity = 0;
+  turnLightAction.style.display = "block";
+  knockAction.style.display = "none";
+  imageContainer.style.opacity = 0;
+  doors.src = "./Bunnydrop/d1.png";
+  wakeAction.style.opacity = 0;
+  wakeAction.style.display = "none";
+  regretAction.style.opacity = 0;
+  regretAction.style.display = "none";
+  apologizeAction.style.opacity = 0;
+  apologizeAction.style.display = "none";
+  endAction.style.opacity = 0;
+  endAction.style.display = "none";
+  omgAction.style.opacity = 0;
+  omgAction.style.display = "none";
+  body.style.background = "#1b1b1b";
 }
